318025e4b7919008713cbfb57c10217f9a1c1600
Atul1901 <kumar.atul.20299@gmail.com>
2024-02-23 13:10:27 +0530

    completed with the code

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,42 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export function Result({ input }) {
   const result = calculateInvestmentResults(input);
+  const initialInvestment =
+    result[0].valueEndOfYear - result[0].interest - result[0].annualInvestment;
+
   console.log("result", result);
-  return <p>Results...</p>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment</th>
+          <th>Interest(Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {result.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
+          const totalAmount = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmount)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
